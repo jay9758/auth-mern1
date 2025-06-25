76f45dd1bc6b5806dd7609f42d0b1f86ef092e95
jay9758 <163132972+jay9758@users.noreply.github.com>
2025-06-25 12:09:16 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
       return handleError("All fields are required");
     }
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://auth-mern1-api-zeta.vercel.app/auth/login";
       const response=await fetch(url,{
         method:"POST",
         headers:{
