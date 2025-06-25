02ff957d1a67bee6f2449f180430a76af5afd764
jay9758 <163132972+jay9758@users.noreply.github.com>
2025-06-25 12:09:57 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
   const fetchProducts=async()=>{
     try {
-      const url="http://localhost:8080/products";
+      const url="https://auth-mern1-api-zeta.vercel.app/products";
       const headers={
         headers:{
           'Authorization' : localStorage.getItem('token')
