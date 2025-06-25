6bc46f94cd63d9e55d3593fd13f4402d4ee238cc
jay9758 <163132972+jay9758@users.noreply.github.com>
2025-06-25 12:08:31 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ const Signup = () => {
       return handleError("All fields are required");
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://auth-mern1-api-zeta.vercel.app/auth/signup";
       const response=await fetch(url,{
         method:"POST",
         headers:{
